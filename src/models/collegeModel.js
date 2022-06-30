aebc9e2630ac110a2eb322650bd468a42efe491b
ShivanandKasture <shivanandkasture1415@gmail.com>
2022-07-01 00:03:49 +0530

    add

--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -10,8 +10,7 @@ const collegeSchema = new mongoose.Schema({
     },
     fullName: {
         type: String,
-        required: true,
-        trim: true
+        required: true
     },
     logoLink: {
         type: String,
